9639b2721322060908a01cf9e9371607622724ed
k.rakitskaya <katerina.taradanova@gmail.com>
2025-04-07 00:06:41 +0200

    refactoring

--- src/test/java/com/kate/project/api/BaseApiTest.java
+++ src/test/java/com/kate/project/api/BaseApiTest.java
@@ -5,5 +5,5 @@ import com.kate.project.api.interfaces.ResponseVerifier;
 import com.kate.project.factory.ApiClientFactory;
 
 public abstract class BaseApiTest extends BasePlaywrightTest implements ResponseVerifier {
-    protected static ApiClientFactory defaultApiClientFactory = new ApiClientFactory();
+    protected static final ApiClientFactory defaultApiClientFactory = new ApiClientFactory();
 }
